541c6b36a3c1d7623a9da6ab2fc3f5c71ea7b414
ohtaekwon <ohtk1119@gmail.com>
2023-04-12 23:30:51 +0900

    Chore mongoose 버전에 따른 옵션 추가 및 라우터 연결

--- server/index.js
+++ server/index.js
@@ -4,20 +4,25 @@ import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
+import routes from "./src/routes/index.js";
 
 const app = express();
+const { PORT, MONGO_DB_URL } = process.env;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const port = process.env.PORT || 8000;
+app.use("/api/v1", routes);
+
+const port = PORT || 8000;
 const server = http.createServer(app);
+
 mongoose
-  .connect(process.env.MONGOOSE_DB_URL)
+  .connect(MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("MongoDB Connected");
+    console.log("Successfully connected to mongodb");
     server.listen(port, () => {
       console.log(`Server is listening on port ${port}`);
     });
