4ab1e6f9d8bba76d353fc38876c838a9d73505db
ohtaekwon <ohtk1119@gmail.com>
2023-04-12 23:29:24 +0900

    Feat Review Conroller 구현

---
+++ server/src/controller/review.controller.js
@@ -0,0 +1,68 @@
+import responseHandler from "../handlers/response.handler.js";
+import reviewModel from "../models/review.model.js";
+
+const create = async (req, res) => {
+  try {
+    const {
+      params: { movieId },
+      user: { id },
+      body,
+    } = req;
+    const review = new reviewModel({
+      user: id,
+      movieId,
+      ...body,
+    });
+    await review.save();
+
+    responseHandler.created(res, {
+      ...review._doc,
+      id: review.id,
+      user: req.user,
+    });
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const {
+      params: { reviewId },
+      user: { id },
+    } = req;
+
+    const review = await reviewModel.findOne({
+      _id: reviewId,
+      user: id,
+    });
+
+    if (!review) return responseHandler.notFound(res);
+
+    await review.remove();
+
+    responseHandler.ok(res);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const getReviewOfUser = async (req, res) => {
+  try {
+    const {
+      user: { id },
+    } = req;
+
+    const review = await reviewModel
+      .find({
+        user: id,
+      })
+      .sort("-createdAt");
+
+    responseHandler.ok(res, review);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+export default { create, remove, getReviewOfUser };
