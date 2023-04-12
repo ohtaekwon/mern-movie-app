cb62ea6e4a57dce5cecde9236aecfd9126240f1d
ohtaekwon <ohtk1119@gmail.com>
2023-04-12 14:37:15 +0900

    Feat tmdb API 라우터

---
+++ server/src/tmdb/tmdb.config.js
@@ -0,0 +1,10 @@
+const baseUrl = `${process.env.TMDB_BASE_URL}/${process.env.TMDB_VERSION}`;
+const key = process.env.TMDB_KEY;
+
+const getUrl = (endpoint, param) => {
+  const qs = new URLSearchParams(param); // URL 쿼리 문자열을 쓰는데 사용
+
+  return `${baseUrl}/${endpoint}?api_key=${key}&${qs}`;
+};
+
+export default { getUrl };
