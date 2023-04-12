848b405d2c697bc8e13f61e53a2105e31c72a274
ohtaekwon <ohtk1119@gmail.com>
2023-04-12 14:44:23 +0900

    Feat user 컨트롤러

---
+++ server/src/controller/user.controller.js
@@ -0,0 +1,109 @@
+import userModel from "../models/user.model.js";
+import userModel from "../models/user.model.js";
+import jsonwebtoken from "jsonwebtoken";
+import responseHandler from "../handlers/response.handler.js";
+
+const signUp = async (req, res) => {
+  try {
+    const {
+      body: { username, password, displayName },
+    } = req;
+
+    const checkUser = await userModel.findOne({ username });
+
+    if (checkUser) {
+      return responseHandler.badRequest(res, "username already used");
+    }
+    const user = new userModel();
+    user.username = username;
+    user.setPassword(password);
+
+    await user.saver();
+
+    const token = jsonwebtoken.sign(
+      { data: user.id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    responseHandler.created(res, {
+      token,
+      ...user._doc,
+      id: user.id,
+    });
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+const signIn = async (req, res) => {
+  try {
+    const {
+      body: { username, password },
+    } = req;
+
+    const user = await userModel
+      .findOne({ username })
+      .select("username password salt id displayName");
+
+    if (!user) return responseHandler.badRequest(res, "User not exist");
+
+    if (!user.validPassword(password)) {
+      return responseHandler.badRequest(res, "Wrong password");
+    }
+
+    const token = jsonwebtoken.sign(
+      { data: user.id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+    user.password = undefined;
+    user.salt = undefined;
+
+    responseHandler.created(res, {
+      token,
+      ...user._doc,
+      id: user.id,
+    });
+  } catch {
+    responseHandler.error(res);
+  }
+};
+const updatePassword = async (req, res) => {
+  try {
+    const {
+      body: { password, newPassword },
+      user: { id },
+    } = req;
+
+    const user = await userModel.findById(id).select("password id salt");
+    if (!user) return responseHandler.unauthorize(res);
+    if (!user.validPassword(password)) {
+      return responseHandler.badRequest(res, "Wrong password");
+    }
+    user.setPassword(newPassword);
+    await user.save(); // 데이터를 저장
+    responseHandler.ok(res);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+const getInfo = async (req, res) => {
+  try {
+    const {
+      user: { id },
+    } = req;
+    const user = await userModel.findById(id);
+    if (!user) return responseHandler.notFound(res);
+    responseHandler.ok(res, user);
+  } catch {
+    responseHandler.error(res);
+  }
+};
+
+export default {
+  signIn,
+  signUp,
+  getInfo,
+  updatePassword,
+};
