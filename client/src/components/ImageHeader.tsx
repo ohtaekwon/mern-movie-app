f24d59df8156eb8100afb8a59405223737b668d4
ohtaekwon <ohtk1119@gmail.com>
2023-04-21 20:55:17 +0900

    Feat media Slide, Image Poster Head

---
+++ client/src/components/ImageHeader.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import uiConfigs from "configs/ui.config";
+import { Box, useTheme } from "@mui/material";
+
+const ImageHeader = ({ imgPath }: { imgPath: string }) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={{
+        zIndex: "-1",
+        position: "relative",
+        paddingTop: { xs: "60%", sm: "40%", md: "35%" },
+        backgroundPosition: "top",
+        backgroundSize: "cover",
+        backgroundImage: `url(${imgPath})`,
+        backgroundAttachment: "fixed",
+
+        "&::before": {
+          content: '""',
+          position: "absolute",
+          left: 0,
+          bottom: 0,
+          width: "100%",
+          height: "100%",
+          pointerEvents: "none",
+          ...uiConfigs.style.gradientBgImage[theme.palette.mode],
+        },
+      }}
+    ></Box>
+  );
+};
+
+export default ImageHeader;
---
+++ client/src/components/MediaVideoSlide.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Box } from "@mui/material";
+import tmdbConfig from "lib/api/config/tmdb.config";
+import { SwiperSlide } from "swiper/react";
+import NavigationSwiper from "./NavigationSwiper";
+
+const MediaVideo = ({ video }: { video: any }) => {
+  const iframeRef = React.useRef<HTMLIFrameElement>(null);
+
+  React.useEffect(() => {
+    console.log("iframe");
+    const newHeight = (iframeRef.current?.offsetWidth! * 9) / 16 + "px";
+    iframeRef.current?.setAttribute("height", newHeight);
+  }, [video]);
+
+  return (
+    <Box sx={{ height: "max-content" }}>
+      <iframe
+        key={video.key}
+        src={tmdbConfig.youtubePath(video.key)}
+        ref={iframeRef}
+        width="100%"
+        title={video.id}
+        style={{ border: 0 }}
+      ></iframe>
+    </Box>
+  );
+};
+
+const MediaVideoSlide = ({ videos }: { videos: any }) => {
+  console.log({ videos });
+
+  return (
+    <>
+      <NavigationSwiper>
+        {videos.map((video: any, index: number) => (
+          <SwiperSlide key={index}>
+            <MediaVideo video={video} />
+          </SwiperSlide>
+        ))}
+      </NavigationSwiper>
+    </>
+  );
+};
+
+export default MediaVideoSlide;
