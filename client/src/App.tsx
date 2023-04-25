2e498e52f01950929aaf99cf659ceb9160544d25
ohtaekwon <ohtk1119@gmail.com>
2023-04-25 18:24:27 +0900

    Chore connect router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,82 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
-function App() {
-  return <div className="App"></div>;
-}
+import themeConfigs from "configs/theme.config";
+import { RootState } from "redux/store";
+import routes from "routes/router";
+import PageWrapper from "components/PageWrapper";
+import Layout from "components/Layout";
+
+import "react-toastify/dist/ReactToastify.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const App = () => {
+  const { themeMode } = useSelector((state: RootState) => state.themeMode);
+
+  return (
+    <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
+      {/* Toastify config */}
+      <ToastContainer
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnFocusLoss
+        pauseOnHover
+        theme={themeMode}
+      />
+      {/* mui reset css */}
+      <CssBaseline />
+
+      {/* app routes */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {routes.map((route, index) =>
+              // 경로가 인덱스 경로인지 확인합니다. 인덱스 경로는 부모의 URL에서 부모의 Outlet 으로 렌더링하게 된다.
+              route.index ? (
+                <Route
+                  index
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              ) : (
+                <Route
+                  path={route.path}
+                  key={index}
+                  element={
+                    route.state ? (
+                      <PageWrapper state={route.state}>
+                        {route.element}
+                      </PageWrapper>
+                    ) : (
+                      route.element
+                    )
+                  }
+                />
+              )
+            )}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* app routes */}
+    </ThemeProvider>
+  );
+};
 
 export default App;
