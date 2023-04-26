5f706fb07708bb08f98632100e04313b81958659
ohtaekwon <ohtk1119@gmail.com>
2023-04-26 21:38:10 +0900

    Chore add url for kr language

--- server/src/tmdb/tmdb.config.js
+++ server/src/tmdb/tmdb.config.js
@@ -4,7 +4,7 @@ const key = process.env.TMDB_KEY;
 const getUrl = (endpoint, param) => {
   const qs = new URLSearchParams(param); // URL 쿼리 문자열을 쓰는데 사용
 
-  return `${baseUrl}/${endpoint}?api_key=${key}&${qs}`;
+  return `${baseUrl}/${endpoint}?api_key=${key}&${qs}&include_image_language="kr,en,null`;
 };
 
 export default { getUrl };
