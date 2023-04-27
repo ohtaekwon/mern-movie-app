ec034fa5a7d0cf48becb95c38f768816eac58383
ohtaekwon <ohtk1119@gmail.com>
2023-04-27 18:51:54 +0900

    Chore Add Review, Video, User, Cast Types

--- client/src/types/index.types.ts
+++ client/src/types/index.types.ts
@@ -1,3 +1,11 @@
+export interface User {
+  displayName: string;
+  username: string;
+  createdAt: string;
+  updatedAt: string;
+  id: string;
+}
+
 export interface MediaType {
   adult?: boolean;
   genre_ids: number[];
@@ -24,11 +32,16 @@ export interface totalMediaProps extends MediaType {
   first_air_date?: string;
   mediaRate?: number;
 }
-
+/**
+ * @description 장르
+ */
 export type GenresType = {
   id: number;
   name: string;
 };
+/**
+ * @description 영화 포스터
+ */
 
 export interface Poster {
   aspect_ratio: number;
@@ -39,7 +52,9 @@ export interface Poster {
   vote_count: number;
   width: number;
 }
-
+/**
+ * @description 배경 이미지
+ */
 export interface BackDrop {
   aspect_ratio: number;
   height: number;
@@ -48,4 +63,50 @@ export interface BackDrop {
   vote_average: number;
   vote_count: number;
   width: number;
+}
+/**
+ * @description 출연 배우
+ */
+
+export interface Cast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string;
+  cast_id: number;
+  character: string;
+  credit_id: string;
+  order: number;
+}
+/**
+ * @description 비디오 타입
+ */
+export interface Video {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: "YouTube";
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
+}
+
+export interface Review extends User {
+  user: User;
+  content: string;
+  mediaType: "movie" | "tv";
+  mediaId: string | number;
+  mediaTitle: string;
+  mediaPoster: string;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+  id: string;
 }
