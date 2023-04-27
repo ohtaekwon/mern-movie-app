b03e9f9c39856eee98fb07830a7ea2373549bf22
ohtaekwon <ohtk1119@gmail.com>
2023-04-27 18:51:01 +0900

    Feat Add avatar component for reivew user icon

---
+++ client/src/components/TextAvatar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Avatar } from "@mui/material";
+
+const TextAvatar = ({ text }: { text: string }) => {
+  const stringToColor = (str: string) => {
+    let hash = 0;
+    let i;
+
+    for (i = 0; i < str.length; i += 1) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      // 16진수 0xff = 255
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+
+    return color;
+  };
+
+  // console.log("컬러", stringToColor(text));
+  return (
+    <Avatar
+      sx={{
+        backgroundColor: stringToColor(text),
+        width: 40,
+        height: 40,
+      }}
+      children={`${text.split(" ")[0][0]}`}
+    />
+  );
+};
+
+export default TextAvatar;
