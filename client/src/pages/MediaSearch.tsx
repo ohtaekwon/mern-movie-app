ea318d73b490f5408a5e90b6d875e993f460e155
ohtaekwon <ohtk1119@gmail.com>
2023-04-28 20:47:39 +0900

    Feat Add search page

--- client/src/pages/MediaSearch.tsx
+++ client/src/pages/MediaSearch.tsx
@@ -1,5 +1,149 @@
 import React from "react";
 
-export default function MediaSearch() {
-  return <div>MediaSearch</div>;
-}
+import useMultipleState from "hooks/useMultipleState";
+import mediaApis from "lib/api/modules/media.api";
+
+import { Box, Button, Stack, TextField, Toolbar } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+
+import { toast } from "react-toastify";
+import uiConfigs from "configs/ui.config";
+import MediaGrid from "components/MediaGrid";
+
+let timer: NodeJS.Timeout;
+const timeout = 500;
+
+const mediaTypes = ["movie", "tv", "people"] as const;
+const STATE_NAMES = {
+  query: "query",
+  onSearch: "onSearch",
+  mediaTypes: "mediaType",
+  medias: "medias",
+  page: "page",
+};
+
+const MediaSearch = () => {
+  const [{ query, onSearch, mediaType, medias, page }, setSearchState] =
+    useMultipleState({
+      query: "" as string,
+      onSearch: false as boolean,
+      mediaType: mediaTypes[0] as "movie" | "tv" | "people",
+      medias: [],
+      page: 1 as number,
+    });
+
+  const search = React.useCallback(async () => {
+    setSearchState({ name: STATE_NAMES.onSearch, value: true });
+
+    const { response, error } = await mediaApis.search({
+      mediaType,
+      query,
+      page,
+    });
+
+    console.log("response", response);
+    setSearchState({
+      name: STATE_NAMES.onSearch,
+      value: false,
+    });
+
+    if (error) toast.error(error.message);
+    if (response) {
+      if (page > 1)
+        setSearchState({
+          name: STATE_NAMES.medias,
+          value: [...medias, ...response.results],
+        });
+      else
+        setSearchState({
+          name: STATE_NAMES.medias,
+          value: [...response.results],
+        });
+    }
+  }, [mediaType, query, page]);
+
+  React.useEffect(() => {
+    if (query.trim().length === 0) {
+      setSearchState({ name: STATE_NAMES.medias, value: [] });
+      setSearchState({ name: STATE_NAMES.page, value: 1 });
+    } else search();
+  }, [search, query, mediaType, page]);
+
+  React.useEffect(() => {
+    setSearchState({ name: STATE_NAMES.medias, value: [] });
+    setSearchState({ name: STATE_NAMES.page, value: 1 });
+  }, [mediaType]);
+
+  const onCategoryChange = (selectedCategory: "movie" | "tv" | "people") => {
+    setSearchState({ name: STATE_NAMES.mediaTypes, value: selectedCategory });
+  };
+
+  const onQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newQuery = e.target.value;
+    console.log(newQuery);
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      setSearchState({
+        name: STATE_NAMES.query,
+        value: newQuery,
+      });
+    }, timeout);
+  };
+
+  console.log(medias);
+  return (
+    <>
+      <Toolbar />
+      <Box sx={{ ...uiConfigs.style.mainContent }}>
+        <Stack spacing={2}>
+          <Stack
+            spacing={2}
+            direction="row"
+            justifyContent="center"
+            sx={{ width: "100%" }}
+          >
+            {mediaTypes.map((item, index) => (
+              <Button
+                size="large"
+                key={index}
+                variant={mediaType === item ? "contained" : "text"}
+                sx={{
+                  color:
+                    mediaType === item
+                      ? "primary.contrastText"
+                      : "text.primary",
+                }}
+                onClick={() => onCategoryChange(item)}
+              >
+                {item}
+              </Button>
+            ))}
+          </Stack>
+          <TextField
+            color="success"
+            placeholder="Search MoonFlix"
+            sx={{ width: "100%" }}
+            autoFocus
+            onChange={onQueryChange}
+          />
+
+          <MediaGrid medias={medias} mediaType={mediaType} />
+
+          {medias.length > 0 && (
+            <LoadingButton
+              loading={onSearch}
+              onClick={() =>
+                setSearchState({ name: STATE_NAMES.page, value: page + 1 })
+              }
+            >
+              load more
+            </LoadingButton>
+          )}
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default MediaSearch;
